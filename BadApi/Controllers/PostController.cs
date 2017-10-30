9eb9204825edf9ae7b676edcaa3c16dcffc8099d
Joao Lourenco <joao.lourenco@rokt.com>
2017-10-31 09:12:00 +1100

    Added create/get post

---
+++ BadApi/Controllers/PostController.cs
@@ -0,0 +1,54 @@
+﻿using BadApi.Controllers.Base;
+using BadApi.Domain;
+using BadApi.Models.Post;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BadApi.Controllers
+{
+	[RoutePrefix("api/post")]
+    public class PostController : BaseApiController
+    {
+		[HttpGet]
+		[Route("")]
+		public async Task<Post> GetPost(long id)
+		{
+			try
+			{
+				using (var connection = GetConnection())
+				{
+					return await connection.QueryFirstAsync<Post>(string.Format("SELECT * FROM dbo.Post WHERE PostId = {0}", id));
+				}
+			}
+			catch (Exception ex)
+			{
+				return new Post { PostId = -1, AccountId = -1, Content = "No post found", DateTimeCreatedUtc = DateTime.UtcNow };
+			}
+		}
+
+		[Route("create")]
+		[HttpPost]
+		public async Task<long> CreatePost([FromBody] CreatePostModel model)
+		{
+			try
+			{
+				using (var connection = GetConnection())
+				{
+					model.DateTimeCreatedUtc.Subtract(TimeZoneInfo.Local.BaseUtcOffset); // Adjust the timezone from the client to UTC
+					return await connection.ExecuteScalarAsync<long>(string.Format("INSERT INTO dbo.Post (AccountId, Content, DateTimeCreatedUtc) VALUES ({0}, '{1}', '{2:yyyy-MM-dd HH:mm:ss}')", model.AccountId, model.Content, model.DateTimeCreatedUtc));
+				}
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+				return -1;
+			}
+		}
+    }
+}
