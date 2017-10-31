3847ccac997d5607515f244e8cb4b34440980251
Joao Lourenco <joao.lourenco@rokt.com>
2017-11-01 09:34:44 +1100

    Added top posts

--- BadApi/Models/Account/AccountModel.cs
+++ BadApi/Models/Account/AccountModel.cs
@@ -17,5 +17,37 @@ namespace BadApi.Models.Account
 				return Task.Run(() => AccountHelper.Instance.GetAccountPosts(AccountId, 0, 10)).GetAwaiter().GetResult();
 			}
 		}
+
+		public IEnumerable<ViewPostModel> Top5Posts
+		{
+			get
+			{
+				var allPosts = Task.Run(() => AccountHelper.Instance.GetAccountPosts(AccountId, 0, int.MaxValue))
+					.GetAwaiter()
+					.GetResult()
+					.ToList();
+
+				// Get the top 5 posts by positivity
+				var postsRequired = 5;
+				var topPosts = new List<ViewPostModel>();
+				while (postsRequired > 0 && allPosts.Any())
+				{
+					ViewPostModel post = null;
+					foreach (var p in allPosts)
+					{
+						if (post == null || post.Positives < p.Positives)
+						{
+							post = p;
+						}
+					}
+
+					allPosts.Remove(post);
+					topPosts.Add(post);
+					postsRequired--;
+				}
+
+				return topPosts;
+			}
+		}
 	}
 }
